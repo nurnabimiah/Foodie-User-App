228d940bf5ee3016b860d3d5764ef8a0afb9f593
md.nurnabimiah <diunayonahmed@gmail.com>
2023-11-13 23:36:42 +0600

    added :notification design pattern (done)

--- lib/view/screen/notification_screen/notification_screen.dart
+++ lib/view/screen/notification_screen/notification_screen.dart
@@ -3,6 +3,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodie_resturent_user_app/utils/app_const_file.dart';
 
 import '../../../utils/app_color_resources/app_color_resources.dart';
 import '../../../utils/app_style.dart';
@@ -22,20 +23,127 @@ class NotificationScreen extends StatelessWidget {
       ),
 
 
-      body: Container(
-        color:AppColorResources.primaryColor.withOpacity(0.67),
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-           SizedBox(height: 10.h,) ,
-           Text('Today',style: myStyleInter(14.sp, AppColorResources.primaryBlack,FontWeight.w700)),
+      body: SingleChildScrollView(
+        child: Container(
+          color: AppColorResources.primaryColor.withOpacity(0.67),
+          width: double.infinity,
+          //height: MediaQuery.of(context).size.height,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10.h,),
+              Padding(
+                padding:  EdgeInsets.only(left: 12.0.w,top: 10.h),
+                child: Text('Today', style: myStyleInter(14.sp, AppColorResources.primaryBlack, FontWeight.w700)),
+              ),
+
+              Container(
+
+                color: AppColorResources.primaryWhiteColor,
+
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 3,
+                  separatorBuilder: (BuildContext, index) => Divider(),
+                  itemBuilder: (BuildContext, index) {
+                    return ListTile(
+                      leading: Stack(
+                        children: [
+                          Image.network(
+                            'https://nutritionaustralia.org/app/uploads/2021/08/shutterstock_623425481.jpg',
+                            height: 70.h,
+                            width: 70.w,
+                          ),
+
+                          Positioned(
+                            left: 0.w,
+                            top: 0.h,
+                            child: Container(
+                              height: 8.h,
+                              width: 8.w,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: AppColorResources.primaryPinkColor,
+                              ),
+                            ),
+                          ),
+
+                        ],
+                      ),
+
+                      title: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(AppConstFile.notificationTitle,style:myStyleInter(14.sp, AppColorResources.subBlackColor,FontWeight.w600),),
+                          Text(AppConstFile.notificationSubtitle,
+                              style:TextStyle(fontSize: 12.sp,color:AppColorResources.subBlackColor,fontWeight:FontWeight.w400 ),textAlign: TextAlign.justify,),
+                        ],
+                      ),
+                      // title: Text(AppConstFile.notificationTitle,style:myStyleInter(14.sp, AppColorResources.subBlackColor,FontWeight.w600),),
+                      // subtitle: Text(AppConstFile.notificationSubtitle,style:TextStyle(fontSize: 12.sp,color:AppColorResources.subBlackColor,fontWeight:FontWeight.w400 )),
+                    );
+                  },
+                ),
 
 
+              ),
 
-          ],
+
+            ///...............this week.........................................
+
+              Padding(
+                padding:  EdgeInsets.only(left: 12.0.w,top: 10.h),
+                child: Text('This Week', style: myStyleInter(14.sp, AppColorResources.primaryBlack, FontWeight.w700)),
+              ),
+              SizedBox(height: 10.h,),
+              Container(
+                color: AppColorResources.primaryWhiteColor,
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 4,
+                  separatorBuilder: (BuildContext, index) => Divider(),
+                  itemBuilder: (BuildContext, index) {
+                    return ListTile(
+                      leading: Image.network('https://nutritionaustralia.org/app/uploads/2021/08/shutterstock_623425481.jpg', height: 70.h, width: 70.w,),
+                      title: Text(AppConstFile.notificationTitle,),
+                      subtitle: Text(AppConstFile.notificationSubtitle,),
+                    );
+                  },
+                ),
+              ),
+
+
+              ///...............this month.......................................
+
+              Padding(
+                padding:  EdgeInsets.only(left: 12.0.w,top: 10.h),
+                child: Text('This month', style: myStyleInter(14.sp, AppColorResources.primaryBlack, FontWeight.w700)),
+              ),
+              SizedBox(height: 10.h,),
+              Container(
+                color: AppColorResources.primaryWhiteColor,
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 2,
+                  separatorBuilder: (BuildContext, index) => Divider(),
+                  itemBuilder: (BuildContext, index) {
+                    return ListTile(
+                      leading: Image.network('https://nutritionaustralia.org/app/uploads/2021/08/shutterstock_623425481.jpg', height: 70.h, width: 70.w,),
+                      title: Text(AppConstFile.notificationTitle,),
+                      subtitle: Text(AppConstFile.notificationSubtitle,),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
+
+
 
 
     );
