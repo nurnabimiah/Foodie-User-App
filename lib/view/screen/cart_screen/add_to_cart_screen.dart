8bb179c81a2dec808c5ac7f0e99da7416a163628
md.nurnabimiah <diunayonahmed@gmail.com>
2023-11-30 22:58:46 +0600

    modify: my cart screen

--- lib/view/screen/cart_screen/add_to_cart_screen.dart
+++ lib/view/screen/cart_screen/add_to_cart_screen.dart
@@ -4,13 +4,16 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodie_resturent_user_app/view/widget/button_widget/custom_button_widget.dart';
 import '../../../utils/app_color_resources/app_color_resources.dart';
 import '../../../utils/app_style.dart';
 
 class AddToCartScreen extends StatelessWidget {
   static const String routeName = '/addto_cart_route';
 
-  const AddToCartScreen({Key? key}) : super(key: key);
+  AddToCartScreen({Key? key}) : super(key: key);
+
+  final couponCodeController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +30,8 @@ class AddToCartScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
+            //................... add to cart item  container...................
+            SizedBox(height: 10.h,),
             Container(
               color: AppColorResources.primaryWhiteColor,
               child: ListView.separated(
@@ -41,32 +46,85 @@ class AddToCartScreen extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         Flexible(
-                          child: CachedNetworkImage(
-                            fit: BoxFit.fitHeight,
-                            width: double.infinity,
-                            height: 80.h,
-                            imageUrl: 'https://img.freepik.com/premium-photo/fried-chicken-with-french-fries-nuggets-meal-junk-food-unhealthy-food_1339-120123.jpg',
-                            progressIndicatorBuilder: (context, url, downloadProgress) =>
-                                Image.asset("assets/images/place_holder_image.png", fit: BoxFit.fill),
-                            errorWidget: (context, url, error) =>
-                                Image.asset("assets/images/placeholder.jpg", fit: BoxFit.fill),
+                          child: Padding(
+                            padding:  EdgeInsets.only(bottom: 8.0.h),
+                            child: CachedNetworkImage(
+                              fit: BoxFit.fitHeight,
+                              width: 100.w,
+                              height: 80.h,
+                              imageUrl: 'https://img.freepik.com/premium-photo/fried-chicken-with-french-fries-nuggets-meal-junk-food-unhealthy-food_1339-120123.jpg',
+                              progressIndicatorBuilder: (context, url, downloadProgress) =>
+                                  Image.asset("assets/images/place_holder_image.png", fit: BoxFit.fill),
+                              errorWidget: (context, url, error) =>
+                                  Image.asset("assets/images/placeholder.jpg", fit: BoxFit.fill),
+                            ),
                           ),
                         ),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text('Sandwich', style: myStyleInter(14.sp, AppColorResources.subBlackColor, FontWeight.w600)),
-                            Text('\$2.2', style: myStyleInter(14.sp, AppColorResources.subBlackColor, FontWeight.w500)),
-                          ],
-                        ),
-                        Spacer(),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text('Sandwich', style: myStyleInter(14.sp, AppColorResources.subBlackColor, FontWeight.w600)),
+                            Row(
+                              children: [
+                                Text('\$2.2', style: myStyleInter(14.sp, AppColorResources.subBlackColor, FontWeight.w500)),
+                                SizedBox(width: 120.w,),
 
+
+                                Container(
+                                  height: 30.h,
+                                  width: 90.w,
+                                  decoration: BoxDecoration(
+                                    color: AppColorResources.primaryPinkColor,
+                                    borderRadius: BorderRadius.all(Radius.circular(12.r)),
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(horizontal: 8.0.w),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        GestureDetector(
+                                          onTap: () {
+                                            // setState(() {
+                                            //   _controller.removeFromCart(widget.product);
+                                            // });
+                                          },
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                              color: Colors.white.withOpacity(0.7),
+                                              shape: BoxShape.circle,
+                                            ),
+                                            child: Icon(Icons.remove, size: 20),
+                                          ),
+                                        ),
+
+                                        Text('1', style: TextStyle(fontSize: 20)),
+
+                                        GestureDetector(
+                                          onTap: () {
+                                            // setState(() {
+                                            //   _controller.addToCart(widget.product);
+                                            //
+                                            // });
+                                          },
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                              color: Colors.white.withOpacity(0.7),
+                                              shape: BoxShape.circle,
+                                            ),
+                                            child: Icon(Icons.add, size: 20),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+
+                              ],
+                            )
                           ],
                         ),
+
+
 
                       ],
                     ),
@@ -74,18 +132,49 @@ class AddToCartScreen extends StatelessWidget {
                 },
               ),
             ),
+            SizedBox(height: 10.h,),
 
+
+            //.................coupon code container............................
             Container(
-              height: 50.h, // Adjust the height as needed
-              color: AppColorResources.primaryColor,
+              height: 100.h, // Adjust the height as needed
+              color: AppColorResources.primaryWhiteColor,
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      'Order Summary',
-                      style: myStyleInter(14.sp, AppColorResources.subBlackColor, FontWeight.w600),
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        height: 50.h,
+                        child: TextFormField(
+
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.only(top: 5.h,left: 12.w),
+                            hintText: 'Enter coupon code',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(4.0),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
+
+                    SizedBox(width: 20.w,),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        height: 50.h,
+                        width: 100.w,
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.all(Radius.circular(4.r))
+                        ),
+                        child: Center(child: Text('Apply',style: myStyleInter(14.sp, AppColorResources.primaryBlack,FontWeight.w600),)),
+                      ),
+                    )
+
                   ],
                 ),
               ),
